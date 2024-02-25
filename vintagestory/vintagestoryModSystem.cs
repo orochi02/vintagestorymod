59860bc5bd2f9a53597a672e74cac8bd6261a72a
02dev <orochi_100@pm.me>
2024-02-25 02:24:00 +0100

    init commit

---
+++ vintagestory/vintagestoryModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace vintagestory;
+
+public class vintagestoryModSystem : ModSystem
+{
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        api.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        api.Logger.Notification("Hello from template mod server side: " + Lang.Get("vintagestory:hello"));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        api.Logger.Notification("Hello from template mod client side: " + Lang.Get("vintagestory:hello"));
+    }
+}
